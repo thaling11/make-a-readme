75dd84a23278f2fc6e8e26ba100120a32a9d1f10
thaling <thaling@verizon.net>
2022-08-17 20:32:53 -0600

    index.js functions and input

--- Develop/index.js
+++ Develop/index.js
@@ -1,20 +1,10 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const generateMarkdown = require("./generateMarkdown.js");
-const fs = require("fs");
+const fs = require('fs');
 
-// TODO: Create a function to write README file
-const writeToFile = {(title, description, installation, usage, contributing, testsInput)} => `
-    #${title}
-    ##${description}
-    ##${installation}
-    ##${usage}
-    ##${contributing}
-    ##
-`
 // TODO: Create an array of questions for user input
-inquirer
-  .prompt([
+const userInput = [
     {
       type: "input",
       message: "What is the title?",
@@ -41,20 +31,27 @@ inquirer
       name: "contributing",
     },
     {
-      type: "input",
-      message: "Tests:",
-      name: "testsInput",
-    },
-  ])
-  .then((response) => {
-    fs.writeFile("README.md", writeToFile(response), (err) =>
-      err ? console.error(err) : console.log("Success!")
-    );
-  });
+        type: "input",
+        message: "Tests:",
+        name: "tests",
+      }
+  ];
+//   .then((response) => {
+//     fs.writeFile('README.md', writeToFile(response), (err) =>
+//     err ? console.error(err) : console.log("Success!"))
+// });
 
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.error(err) : console.log("Success!")
+    })
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(userInput)
+}
 
 // Function call to initialize app
 init();
